479eb91f81ce67a1d3225e1d771fdf2b459e5ae6
blondisb <blondisb@uninorte.edu.co>
2025-01-14 17:27:48 -0500

    shaping companies-bussiness-logic

--- backend/api/routes/companies_router.py
+++ backend/api/routes/companies_router.py
@@ -34,57 +34,19 @@ if stage == "dev":
 
 
 
-@router.post("/companies/", response_model=CompanyResponse)
+@router.post("/newcompany/", response_model=CompanyResponse)
 async def create_company(
     company: CompanyCreate,
     db: AsyncSession = Depends(get_db),
     current_user: User = Depends(get_current_active_user)  # Cambiado de verify_admin
 ):
-    try:
-        # Verificar que el usuario es ADMIN
-        if current_user.role != UserRole.ADMIN:
-            raise HTTPException(
-                status_code=403,
-                detail="Solo los administradores pueden crear empresas"
-            )
+    return await services.create_company_srv(company, db, current_user)
 
-        # Crear grupo en Cognito usando el nombre de la empresa
-        group_name = company.name.lower().replace(" ", "_")
-        cognito_response = cognito_client.create_group(
-            GroupName=group_name,
-            UserPoolId=os.getenv('COGNITO_USER_POOL_ID'),
-            Description=f"Grupo para la empresa de transporte: {company.name}"
-        )
 
-        # Agregar admin al grupo en Cognito
-        cognito_client.admin_add_user_to_group(
-            UserPoolId=os.getenv('COGNITO_USER_POOL_ID'),
-            Username=current_user.email,  # Usar el email del usuario actual
-            GroupName=group_name
-        )
 
-        # Crear la empresa en la base de datos
-        db_company = Company(
-            **company.model_dump(),
-            cognito_group_id=cognito_response['Group']['GroupName'],
-            admin_id=current_user.id  # Usar el ID del usuario actual
-        )
-        
-        db.add(db_company)
-        db.commit()
-        db.refresh(db_company)
-        return db_company
-
-    except Exception as e:
-        print(f"Error creating company: {str(e)}")  # Añadido para debugging
-        # Rollback en caso de error
-        try:
-            if 'group_name' in locals():
-                cognito_client.delete_group(
-                    GroupName=group_name,
-                    UserPoolId=os.getenv('COGNITO_USER_POOL_ID')
-                )
-        except:
-            pass
-        db.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
+@router.get("/companies/", response_model=List[CompanyResponse])
+async def get_companies(
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_active_user)
+):
+    return await services.get_companies_srv(db, current_user)
